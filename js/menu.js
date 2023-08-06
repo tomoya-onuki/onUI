30fcd69768a2e7517ebaca26f6d8e642c2952e58
tomoya-onuki <tomoya.onuki28@gmail.com>
2023-08-06 22:22:39 +0900

    update

---
+++ js/menu.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', () => {
+
+    window.addEventListener('scroll', () => {
+        let flag = false;
+        Array.from(document.querySelectorAll('h2')).reverse().forEach($elem => {
+            const rect = $elem.getBoundingClientRect();
+            const id = $elem.getAttribute('id');
+
+            try {
+                let $box = document.querySelector(`#${id}-menu`);
+                if (!flag && rect.top <= 5) {
+                    flag = true;
+                    $box.querySelector('li').style.color = '#222';
+                    $box.querySelector('li > a').style.color = '#222';
+                    // $box.querySelector('li > a').style.color = '#3d98ff';
+                    $box.querySelector('.sub-menu').style.display = 'block';
+                } else {
+                    $box.querySelector('li > a').style.color = '#aaa';
+                    $box.querySelector('li').style.color = '#aaa';
+                    $box.querySelector('.sub-menu').style.display = 'none';
+                }
+            } catch (e) {
+
+            }
+        });
+    });
+});
